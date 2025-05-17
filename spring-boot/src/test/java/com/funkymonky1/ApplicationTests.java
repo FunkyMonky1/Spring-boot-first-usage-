36d4a031d5087fabf45450ff8bebd5ffcc377159
FunkyMonky1 <159067017+FunkyMonky1@users.noreply.github.com>
2025-05-17 12:12:45 +0200

    Update ApplicationTests.java
    commend is to be ignored

--- spring-boot/src/test/java/com/funkymonky1/ApplicationTests.java
+++ spring-boot/src/test/java/com/funkymonky1/ApplicationTests.java
@@ -2,7 +2,7 @@ package com.funkymonky1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// i am doing this for university this comment is to be ignored
 @SpringBootTest
 class ApplicationTests {
 
